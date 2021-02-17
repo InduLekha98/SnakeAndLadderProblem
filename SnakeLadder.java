c99f1cdab5c2ada555febb04a2b34b3266edfb28
InduLekha98 <rindulekha09@gmail.com>
2021-02-17 23:42:57 +0530

    Added:Rolling die

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,7 +2,10 @@ public class SnakeLadder {
 	final static int PLAYER_1=0;
 	public static void main(String args[])
 	{
- 	  System.out.println("Start the game");
-	  System.out.println("Player1 Position is "+PLAYER_1);
+	  int value;
+	  System.out.println("Start the game");
+	  System.out.println("Player1 Position is"+PLAYER_1);
+	  value = (int)(Math.random() *(6-1+1) +1);
+	  System.out.println("Roll the die ="+ value);
 	}
 }
