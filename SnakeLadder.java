5fce0547001765a60bc047ace72b525d3006f932
InduLekha98 <rindulekha09@gmail.com>
2021-02-18 00:14:35 +0530

    Added:Rolling the die to reach winning 100 position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,15 +1,16 @@
 import java.util.Scanner;
-public class SnakeLadder{
+public class SnakeLadder {
 	final static int LADDER = 1;
 	final static int SNAKE = 2;
 	final static int FINAL_POSITION = 100;
 	final static int INITIAL_POSITION = 0;
 
-	public static void main(String[] args){
+       	public static void main(String[] args){
           int temp=0;
 	  int position=0;
 	  int die=0;
 	  int noPlay=0;
+	  int count=0;
 	  Scanner sc=new Scanner(System.in);
 	  System.out.println("Start the game");
 	  String s="START";
@@ -20,40 +21,43 @@ public class SnakeLadder{
 	    System.out.println("chance ="+play);
 	    if(play==noPlay)
 	    {
-	      System.out.println("NO PLAY");
+               System.out.println("NO PLAY");
 	    }
 	    else if(play==LADDER)
 	    {
-	      die = (int) (Math.random()*(6-1+1)+1);
-	      System.out.println("Die value = "+die);
-	      System.out.println("Ladder");
-	      if(position+die<=100)
-	      {
-	        position=position+die;
-	        temp=position;
-	        System.out.println("Player position = " +position);
-	      }
-	      else
-	      {
-	        System.out.println("Player position = " +position);
-	      }
-	      }
-	      else
-	      {
-	        System.out.println("Snake");
-        	position=position-die;
-        	temp=position;
-        	if(position<0)
-	        {
+	       die = (int) (Math.random()*(6-1+1)+1);
+       	       System.out.println("Die value = "+die);
+	       System.out.println("Ladder");
+	       if(position+die<=100)
+	       {
+	          position=position+die;
+	          temp=position;
+	          System.out.println("Player position = " +position);
+	       }
+	       else
+	       {
+                  System.out.println("Player position = " +position);
+	       }
+	          count=count+1;
+	       }
+         	else
+	       {
+	          System.out.println("Snake");
+	          position=position-die;
+	          temp=position;
+	          if(position<0)
+	       {
 	          position=0;
 	          temp=position;
-                  System.out.println("Player position = " +position);
-	        }
-        	else
-	        {
-	           System.out.println("Player position = " +position);
-	        }
-	        }
-	    }
-	}
+	          System.out.println("Player position = " +position);
+	       }
+	        else
+	       {
+	          System.out.println("Player position = " +position);
+	       }
+	          count=count+1;
+	       }
+               }
+	          System.out.println("Die has rolled for " +count+ " times");
+	       }
 }
